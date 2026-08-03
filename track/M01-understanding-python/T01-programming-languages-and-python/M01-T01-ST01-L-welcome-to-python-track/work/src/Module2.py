21b8524fb1c3d3592bf02710623f308870a793ca
vanipenta-pavitra <164219621+vanipenta-pavitra@users.noreply.github.com>
2026-08-03 18:07:00 +0530

    Day2 Changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/Module2.py
@@ -0,0 +1,9 @@
+student_name = "Arya"
+course_duration = 30
+course_rating = 4.3
+is_enrolled = True
+
+print(type(student_name))
+print(type(course_duration))
+print(type(course_rating))
+print(type(is_enrolled))
