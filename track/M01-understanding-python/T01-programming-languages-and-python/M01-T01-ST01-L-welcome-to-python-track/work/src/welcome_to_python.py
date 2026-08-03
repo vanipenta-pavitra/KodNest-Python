a8e148826acea1dae4bb9d4cd23f0f8bc77d3d86
vanipenta-pavitra <164219621+vanipenta-pavitra@users.noreply.github.com>
2026-08-03 17:24:31 +0530

    Day2changes

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
@@ -1,2 +1,7 @@
 print("Hello Everyone")
 print("Welcome to kodnest future  developers")
+a = int(input("Enter a num"))
+b = int(input("Enter a num2"))
+c = a + b
+print("The sum of two numbers:" , c)
+
