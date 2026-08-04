5f018fed6b0e3f4dc6a740f1717a5c203bb51a99
vanipenta-pavitra <164219621+vanipenta-pavitra@users.noreply.github.com>
2026-08-04 18:48:08 +0530

    Day3 Changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/code1.py
@@ -0,0 +1,9 @@
+#Checking whether a number is positive or negative
+
+num = int(input("Enter a number:"))
+
+if num > 0 :
+    print("The number is positive" , num)
+
+else:
+    print("The number is negative", num)    
